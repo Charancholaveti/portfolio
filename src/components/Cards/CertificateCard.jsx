f2e547b4b436a250d9d96a05b1ee30f28f60ceb2
charancholaveti <cholaveticharan47@gmail.com>
2025-01-07 21:23:44 +0530

    first commit

---
+++ src/components/Cards/CertificateCard.jsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Document = styled.img`
+  display: none;
+  height: 70px;
+  width: fit-content;
+  background-color: #000;
+  border-radius: 10px;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
+
+const Description = styled.div`
+  width: 100%;
+  font-size: 15px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 99};
+  margin-bottom: 10px;
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Span = styled.span`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+`;
+const Card = styled.div`
+    width: 650px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+    padding: 12px 16px;
+    justify-content: space-between;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    transition: all 0.3s ease-in-out;
+    &:hover{
+        box-shadow: 0px 0px 20px rgba(0,0,0,0.2);
+        transform: translateY(-5px);
+    }
+    @media only screen and (max-width: 768px){
+        padding: 10px;
+        gap: 8px;
+        width: 300px;
+    }
+
+    &:hover ${Document}{
+        display: flex;
+    }
+
+    &:hover ${Span}{
+        overflow: visible;
+        -webkit-line-clamp: unset;
+
+    }
+
+    border: 0.1px solid #306EE8;
+    box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+`;
+
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 12px;
+`;
+
+const Body = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Role = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary + 99};
+  @media only screen and (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+const Skills = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 12px;
+  margin-top: -10px;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Skill = styled.a`
+  font-size: 15px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 99};
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const CertificateCard = ({ certificate }) => {
+  return (
+    <Card>
+      <Top>
+        <Body>
+          <Role>{certificate.role}</Role>
+        </Body>
+      </Top>
+      <Description>
+        {certificate?.skills && (
+          <>
+            <br />
+            <Skills>
+              <b>Skills:</b>
+              <ItemWrapper>
+                {certificate?.skills?.map((skill, index) => (
+                  <Skill
+                    key={index}
+                    href={skill.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    • {skill.name}
+                  </Skill>
+                ))}
+              </ItemWrapper>
+            </Skills>
+          </>
+        )}
+      </Description>
+    </Card>
+  );
+};
+
+export default CertificateCard;
